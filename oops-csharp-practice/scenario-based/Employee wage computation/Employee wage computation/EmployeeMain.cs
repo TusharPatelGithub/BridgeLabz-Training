39a82710cf89287762e32a212fd1f0085f2205e7
TusharPatelGithub <tusharpatelramnagar@gmail.com>
2026-01-08 11:24:46 +0530

    added employee wage computation problem

---
+++ oops-csharp-practice/scenario-based/Employee wage computation/Employee wage computation/EmployeeMain.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace Employee_Wage_Computation
+{
+    internal class EmployeeMain
+    {
+        public static void Main(string[] args)
+        {
+
+            EmployeeMenu employeeMenu = new EmployeeMenu();
+            employeeMenu.employeeMenu();
+        }
+    }
+}
